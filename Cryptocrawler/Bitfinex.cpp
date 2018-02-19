1ad2689058694ec4b76b7ac13114f7ce976b14cc
Marek Pekarovič <marek.pekarovic@gmail.com>
2018-02-19 14:47:37 +0100

    added new functions (GETrequest, getTicker, WriteCallback)...

--- Cryptocrawler/Bitfinex.cpp
+++ Cryptocrawler/Bitfinex.cpp
@@ -27,3 +27,51 @@ Bitfinex::~Bitfinex()
 {
 	curl_easy_cleanup(curl);
 }
+
+bool Bitfinex::inSymbols(const std::string &value, const std::vector<std::string> &symbols)
+{
+	return std::find(symbols.begin(), symbols.end(), value) != symbols.end();
+}
+
+int Bitfinex::getTicker(std::string &result, const std::string &symbol)
+{
+	if (!inSymbols(symbol, symbols))
+	{
+		return -40;
+	}
+	std::string endPoint = "/pubticker/" + symbol;
+	std::string params = "";
+	return GETrequest(endPoint, params, result);
+}
+
+size_t Bitfinex::WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+	((std::string*)userp)->append((char*)contents, size * nmemb);
+	return size * nmemb;
+}
+
+int Bitfinex::GETrequest(const std::string &UrlEnd, const std::string &params, std::string &result)
+{
+	if (curl) 
+	{
+
+		std::string fullurl = APIurl + UrlEnd + params;
+		
+		curl = curl_easy_init();
+		curl_easy_setopt(curl, CURLOPT_URL, fullurl.c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+		res = curl_easy_perform(curl);
+
+		if (res != CURLE_OK)
+		{
+			std::cout << "Libcurl error in GET request, code:\n";
+			return res;
+		}
+		return res;
+	}
+	else
+	{
+		return -50;
+	}
+}
--- Cryptocrawler/Bitfinex.hpp
+++ Cryptocrawler/Bitfinex.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <iostream>
 #include <curl/curl.h>
 
 class Bitfinex
@@ -10,10 +11,16 @@ public:
 	explicit Bitfinex(const std::string &accessKey, const std::string &secretKey);
 	~Bitfinex();
 
+	int getTicker(std::string &result, const std::string &symbol);
+
 private:
 	std::string accessKey, secretKey;
-	CURL* curl;
+	CURL *curl;
+	CURLcode res;
 	std::string APIurl;
 	std::vector<std::string> symbols; 
 	std::vector<std::string> currencies; 
+	static bool inSymbols(const std::string &value, const std::vector<std::string> &symbols);
+	int GETrequest(const std::string &UrlEndPoint, const std::string &params, std::string &result);
+	static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
 };
